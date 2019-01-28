eb6631dd0906cdee502ade32268c4cd4fab23058
WebModerna <edgardogalletto@gmail.com>
2019-01-28 11:06:42 -0300

    Corrección de favicones en la sección de Inglés

--- wp-content/themes/en/header.php
+++ wp-content/themes/en/header.php
@@ -25,29 +25,12 @@
 <![endif]-->
 <?php };?>
 
-<!-- <link rel="shortcut icon" href="http://edificarinmobiliaria.com/favicon.ico" type="image/x-icon" /> -->
-<?php if (wp_is_mobile()) { // Favicones para iPad, etc... ?>
-<link rel="apple-touch-icon-precomposed" sizes="57x57" href="http://edificarinmobiliaria.com/apple-touch-icon-57x57.png" />
-<link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://edificarinmobiliaria.com/apple-touch-icon-114x114.png" />
-<link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://edificarinmobiliaria.com/apple-touch-icon-72x72.png" />
-<link rel="apple-touch-icon-precomposed" sizes="144x144" href="http://edificarinmobiliaria.com/apple-touch-icon-144x144.png" />
-<link rel="apple-touch-icon-precomposed" sizes="60x60" href="http://edificarinmobiliaria.com/apple-touch-icon-60x60.png" />
-<link rel="apple-touch-icon-precomposed" sizes="120x120" href="http://edificarinmobiliaria.com/apple-touch-icon-120x120.png" />
-<link rel="apple-touch-icon-precomposed" sizes="76x76" href="http://edificarinmobiliaria.com/apple-touch-icon-76x76.png" />
-<link rel="apple-touch-icon-precomposed" sizes="152x152" href="http://edificarinmobiliaria.com/apple-touch-icon-152x152.png" />
-<?php };?>
-<link rel="icon" type="image/png" href="http://edificarinmobiliaria.com/favicon-196x196.png" sizes="196x196" />
-<link rel="icon" type="image/png" href="http://edificarinmobiliaria.com/favicon-96x96.png" sizes="96x96" />
-<link rel="icon" type="image/png" href="http://edificarinmobiliaria.com/favicon-32x32.png" sizes="32x32" />
-<link rel="icon" type="image/png" href="http://edificarinmobiliaria.com/favicon-16x16.png" sizes="16x16" />
-<link rel="icon" type="image/png" href="http://edificarinmobiliaria.com/favicon-128.png" sizes="128x128" />
-<meta name="application-name" content="<?php bloginfo('name');?>"/>
-<meta name="msapplication-TileColor" content="#FFFFFF" />
-<meta name="msapplication-TileImage" content="http://edificarinmobiliaria.com/mstile-144x144.png" />
-<meta name="msapplication-square70x70logo" content="http://edificarinmobiliaria.com/mstile-70x70.png" />
-<meta name="msapplication-square150x150logo" content="http://edificarinmobiliaria.com/mstile-150x150.png" />
-<meta name="msapplication-wide310x150logo" content="http://edificarinmobiliaria.com/mstile-310x150.png" />
-<meta name="msapplication-square310x310logo" content="http://edificarinmobiliaria.com/mstile-310x310.png" />
+<!-- Los favicones -->
+<link rel="shortcut icon" type="image/x-icon" href="<?php bloginfo('stylesheet_directory');?>/img/favicon.ico" />
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php bloginfo('stylesheet_directory');?>/img/apple-touch-icon-144x144.png" />
+<link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?php bloginfo('stylesheet_directory');?>/img/apple-touch-icon-152x152.png" />
+<link rel="icon" type="image/png" href="<?php bloginfo('stylesheet_directory');?>/img/favicon-32x32.png" sizes="32x32" />
+<link rel="icon" type="image/png" href="<?php bloginfo('stylesheet_directory');?>/img/favicon-16x16.png" sizes="16x16" />
 
 <?php wp_head();?>
 </head>
